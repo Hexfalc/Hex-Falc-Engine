72dc60d8ae37dd9f494f219d27fcbc4408a6f0f5
Hex Falc The Porter <84611854+Hexfalc@users.noreply.github.com>
2022-05-05 15:44:09 +0200

    update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -16,18 +16,25 @@ class FlashingState extends MusicBeatState
 	public static var leftState:Bool = false;
 
 	var warnText:FlxText;
+	var BackgroundWarning:FlxSprite;
+	
 	override function create()
 	{
 		super.create();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
+		//var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		//add(bg);
+		
+		BackgroundWarning = new FlxSprite().loadGraphic(Paths.image('bgwarning'));
+    BackgroundWarning.screenCenter();
+    BackgroundWarning.antialiasing = ClientPrefs.globalAntialiasing;
+    add(BackgroundWarning);
 
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Hey, watch out!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			Press ENTER/A to disable them now or go to Options Menu.\n
+			Press ESCAPE/B to ignore this message.\n
 			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
