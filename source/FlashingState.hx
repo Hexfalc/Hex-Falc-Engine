4bc4fb94d86fe340e9655d2b0bb82482e55c1c51
Hex Falc The Porter <84611854+Hexfalc@users.noreply.github.com>
2022-05-08 07:52:53 +0200

    update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -29,14 +29,24 @@ class FlashingState extends MusicBeatState
     BackgroundWarning.screenCenter();
     BackgroundWarning.antialiasing = ClientPrefs.globalAntialiasing;
     add(BackgroundWarning);
-
+ 
+    #if android
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Hey, watch out!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER/A to disable them now or go to Options Menu.\n
-			Press ESCAPE/B to ignore this message.\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
 			You've been warned!",
 			32);
+		#else
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hey, watch out!\n
+			This Mod contains some flashing lights!\n
+			Press ENTER to disable them now or go to Options Menu.\n
+			Press ESCAPE to ignore this message.\n
+			You've been warned!",
+			32);
+		#end
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
